07192deb3975c6df74be2ff15b3cd68c78a2e0d9
Tetro <itsofirk@gmail.com>
2024-11-19 00:03:13 +0200

    Apparently multiple files are allowed 🥳 created consts.py and puzzle_state.py

--- mmn11/Tiles.py
+++ mmn11/Tiles.py
@@ -4,50 +4,9 @@ description: 8-Tiles Puzzle solver using 4 different algorithms
 """
 import argparse
 
-PUZZLE_SIZE = 3
+from consts import GOAL_STATE
+from puzzle_state import PuzzleState
 
-EMPTY_TILE = 0
-DIRECTIONS = {'Up': (-1, 0), 'Down': (1, 0), 'Left': (0, -1), 'Right': (0, 1)}
-
-
-class PuzzleState:
-    def __init__(self, board: list[int], parent: 'PuzzleState' = None, action: int = None, depth=0, cost=0):
-        """
-        :param board: a list of 9 integers representing the board
-        :param parent: a reference to the parent state
-        :param action: the tile that was moved to get to this state
-        :param depth: how far from the initial state
-        :param cost: g(n) for A* search
-        """
-        self.board = board
-        self.parent = parent
-        self.action = action
-        self.depth = depth
-        self.cost = cost
-
-    def get_successors(self):
-        successors = []
-        empty_index = self.board.index(EMPTY_TILE)
-        moves = []
-
-        row, col = self._get_empty_coords()
-
-
-        for move in DIRECTIONS.values():
-            new_row, new_col = row + move[0], col + move[1]  # "Move" the empty tile
-            if 0 <= new_row < 3 and 0 <= new_col < 3:  # if within boundaries
-                new_index = new_row * 3 + new_col
-                new_board = self.board.copy()
-                new_board[empty_index], new_board[new_index] = new_board[new_index], new_board[empty_index]
-                action = new_board[empty_index]  # The tile that moved into the blank space
-                successors.append(PuzzleState(new_board, self, action, self.depth + 1))
-        return successors
-
-    def _get_empty_index(self):
-        return self.board.index(EMPTY_TILE)
-
-    def _get_empty_coords(self):
-        return divmod(self._get_empty_index(), PUZZLE_SIZE)
 
 def print_board(board: list):
     """
---
+++ mmn11/consts.py
@@ -0,0 +1,5 @@
+PUZZLE_SIZE = 3
+EMPTY_TILE = 0
+
+GOAL_STATE = [EMPTY_TILE, 1, 2, 3, 4, 5, 6, 7, 8]
+DIRECTIONS = {'Up': (-1, 0), 'Down': (1, 0), 'Left': (0, -1), 'Right': (0, 1)}
