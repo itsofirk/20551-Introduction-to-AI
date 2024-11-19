e1dbc6864be9d40766598fea44ce33b5825025c7
Tetro <itsofirk@gmail.com>
2024-11-19 20:54:24 +0200

    make similar to book (pg. 81)

--- mmn11/algorithms.py
+++ mmn11/algorithms.py
@@ -22,27 +22,27 @@ def bfs(start_state: PuzzleState):
     return current_state, nodes_expanded
 
 
-def dls(state: PuzzleState, limit: int, explored: set):
-    if state.board == GOAL_STATE:
-        return state
-    if state.depth >= limit:
-        return None
-    explored.add(state)
-    for child in state.get_successors():
-        if child not in explored:
-            result = dls(child, limit, explored)
-            if result is not None:
-                return result
-    return None
-
+def dls(state: PuzzleState, limit: int):
+    frontier = [state]
+    explored = set()
+    while frontier:
+        current_state = frontier.pop()
+        if current_state.board == GOAL_STATE:
+            return current_state, len(explored)
+        explored.add(current_state)
+        if len(frontier) > limit:
+            return None, len(explored)
+        for child in current_state.get_successors():
+            if child not in explored and child not in frontier:
+                frontier.append(child)
+    return None, len(explored)
 
 def iddfs(start_state: PuzzleState):
     depth = 0
     nodes_expanded = 0
     while True:
-        explored = set()
-        result = dls(start_state, depth, explored)
-        nodes_expanded += len(explored)
+        result, explored = dls(start_state, depth)
+        nodes_expanded += explored
         if result is not None:
             return result, nodes_expanded
         depth += 1
