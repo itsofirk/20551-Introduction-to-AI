8ddb2926264c4c7862317193f2629913e249d09a
Tetro <itsofirk@gmail.com>
2024-12-19 00:44:35 +0200

    added enum

--- mmn13/Reversi.py
+++ mmn13/Reversi.py
@@ -1,8 +1,39 @@
+import argparse
+
+from consts import GameMode
 from game import Game
 
 def main():
+    args = parse_args()
     game = Game()
-    game.play()
+
+    if args.displayAllActions is not None:
+        mode = GameMode.DISPLAY_ALL_ACTIONS
+        parameter = args.displayAllActions
+    elif args.methodical is not None:
+        mode = GameMode.METHODICAL
+        parameter = args.methodical
+    elif args.random is not None:
+        mode = GameMode.RANDOM
+        parameter = args.random
+    elif args.interactive:
+        mode = GameMode.INTERACTIVE
+        parameter = None
+
+    game.play(mode, parameter)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Reversi Game")
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("--displayAllActions", type=int,
+                       help="Display all possible actions after reaching a given total number of disks")
+    group.add_argument("--methodical", type=int, help="Play a given number of moves methodically")
+    group.add_argument("--random", type=int, help="Play a given number of moves randomly")
+    group.add_argument("--interactive", action="store_true", help="Play interactively")
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
     main()
--- mmn13/consts.py
+++ mmn13/consts.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 PLAYER_1 = 'X'
 PLAYER_2 = 'O'
 EMPTY = '-'
@@ -5,3 +7,10 @@ EMPTY = '-'
 # Directions for move checking: Up, Up-Right, Right, Down-Right, Down, Down-Left, Left, Up-Left
 DIRECTIONS = [(-1, 0), (-1, 1), (0, 1), (1, 1),
               (1, 0), (1, -1), (0, -1), (-1, -1)]
+
+
+class GameMode(Enum):
+    DISPLAY_ALL_ACTIONS = 1
+    METHODICAL = 2
+    RANDOM = 3
+    INTERACTIVE = 4
